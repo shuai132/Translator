49c51e84f6a7d6961eabe38bfd5ba33d0de22606
liushuai <liushuai02@bilibili.com>
2021-05-10 15:42:39 +0800

    feat: basic function

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,45 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <textarea v-model="input_str"></textarea>
+    <button @click="onTranslate(input_str)">Translate</button>
+    <div>YouDao: {{ result.youdao }}</div>
+    <div>Google: {{ result.google }}</div>
   </div>
 </template>
 
 <script>
+import {translateYouDao} from "./translator";
+import { youdao, baidu, google } from 'translation-jsapi'
+
 export default {
   name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      input_str: "",
+      result: {
+        youdao: "",
+        google: "",
+        baidu: "",
+        reset() {
+          this.youdao = "";
+          this.google = "";
+          this.baidu = "";
+        }
+      }
+    }
+  },
+  methods: {
+    onTranslate(input) {
+      this.result.reset();
+      translateYouDao(input, (result) => {
+        this.result.youdao = result;
+      });
+
+      google.translate(input).then(result => {
+        console.log(result)
+        this.result.google = result.result[0];
+      })
     }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
