9de7679d84248005796f67f65ab0fed52acc5bcc
liushuai <liushuai02@bilibili.com>
2021-05-11 22:22:32 +0800

    feat: refine UI

--- src/renderer/components/home/index.vue
+++ src/renderer/components/home/index.vue
@@ -1,16 +1,45 @@
 <template>
   <div id="app">
-    <textarea v-model="input_str"></textarea>
-    <button @click="onTranslate(input_str)">Translate</button>
-    <div>YouDao:</div>
-    <div>Result:{{ result.youdao }}</div>
-    <div>YouDao:{{ result.youdao2 }}</div>
-    <div>Google:{{ result.youdao3 }}</div>
-    <p></p>
-    <div>Google:</div>
-    <div>Result:{{ result.google }}</div>
-    <div>YouDao:{{ result.google2 }}</div>
-    <div>Google:{{ result.google3 }}</div>
+    <div>
+      <div>
+        <textarea class="swing" id="input_str" v-model="input_str"></textarea>
+        <label class="swing" for="input_str">INPUT</label>
+      </div>
+      <button id="translate" @click="onTranslate(input_str)">Translate</button>
+    </div>
+
+    <div class="result">
+      <label class="result_head">YouDao:</label>
+      <div class="result_item">
+        <label class="result_item_title">R:</label>
+        <span class="result_item_content">{{ result.youdao }}</span>
+      </div>
+      <div class="result_item">
+        <label class="result_item_title">Y:</label>
+        <span class="result_item_content">{{ result.youdao2 }}</span>
+      </div>
+      <div class="result_item">
+        <label class="result_item_title">G:</label>
+        <span class="result_item_content">{{ result.youdao3 }}</span>
+      </div>
+    </div>
+    <div class="result">
+      <label class="result_head">Google:</label>
+      <div class="result_item">
+        <label class="result_item_title">R:</label>
+        <span class="result_item_content">{{ result.google }}</span>
+      </div>
+      <div class="result_item">
+        <label class="result_item_title">Y:</label>
+        <span class="result_item_content">{{ result.google2 }}</span>
+      </div>
+      <div class="result_item">
+        <label class="result_item_title">G:</label>
+        <span class="result_item_content">{{ result.google3 }}</span>
+      </div>
+    </div>
+
+    <label id="about">Design for TanXingXing Copyright © 2021, All Rights Reserved.</label>
   </div>
 </template>
 
@@ -18,6 +47,7 @@
 import {translateYouDao} from '../js/translator'
 // eslint-disable-next-line no-unused-vars
 import { youdao, baidu, google } from 'translation-jsapi'
+import { Toast } from '../js/utils'
 
 export default {
   name: 'app',
@@ -25,12 +55,12 @@ export default {
     return {
       input_str: '',
       result: {
-        youdao: '',
-        youdao2: '',
-        youdao3: '',
-        google: '',
-        google2: '',
-        google3: '',
+        youdao: 'Result: 有道翻译的结果',
+        youdao2: '用有道翻译Result',
+        youdao3: '用Google翻译Result',
+        google: 'Result: Google翻译的结果',
+        google2: '用有道翻译Result',
+        google3: '用Google翻译Result',
         reset () {
           this.youdao = ''
           this.youdao2 = ''
@@ -39,11 +69,25 @@ export default {
           this.google2 = ''
           this.google3 = ''
         }
+      },
+      state: {
+        lastClickTime: 0
       }
     }
   },
   methods: {
     onTranslate (input) {
+      const now = Date.now()
+      if (now - this.state.lastClickTime < 1000) {
+        Toast('点击太快了吧亲~~  ╮(￣▽ ￣)╭ 我竟无言以对~~', 500)
+        return
+      }
+      this.state.lastClickTime = now
+
+      if (input === '') {
+        Toast('好像什么都没有输入哦亲~~  >_<|||', 500)
+        return
+      }
       this.result.reset()
       translateYouDao(input, (result) => {
         this.result.youdao = result
@@ -57,6 +101,7 @@ export default {
         })
       })
 
+      // eslint-disable-next-line no-unreachable
       google.translate(input).then(result => {
         result = result.result[0]
         this.result.google = result
@@ -73,3 +118,69 @@ export default {
   }
 }
 </script>
+
+<style src="../style/input.css"/>
+
+<style>
+#app{
+  padding: 12px 20px;
+}
+.swing {
+  color: white;
+  font-weight: bold;
+}
+#input_str {
+  width: 100%;
+  padding: 10px 20px;
+  margin: 8px 0;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  font-size: 12pt;
+  font-weight: normal;
+  resize: vertical;
+  color: #3c3c3c;
+}
+
+#translate {
+  background-color: #4CAF50;
+  font-size: 12pt;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.result {
+  background-color: #606060;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+.result_head {
+  font-size: large;
+  font-weight: bold;
+}
+.result_item {
+  margin: 11px 10px;
+}
+.result_item_title {
+  display:inline-block;
+  width: 20px;
+  font-weight: bold;
+}
+.result_item_content {
+}
+#about {
+  color: grey;
+  display: flex;
+  justify-content: center;
+  font-size: 10px;
+  margin-top: 10px;
+}
+</style>
