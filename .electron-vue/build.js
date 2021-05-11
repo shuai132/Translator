43cd63ea430f4205254b30c9820cb8afa3b9cf2d
liushuai <liushuai02@bilibili.com>
2021-05-11 14:57:06 +0800

    feat: fix build

--- .electron-vue/build.js
+++ .electron-vue/build.js
@@ -34,11 +34,6 @@ async function build () {
 
   del.sync(['dist/electron/*', '!.gitkeep'])
 
-  const tasks = ['main', 'renderer']
-  const m = new Multispinner(tasks, {
-    preText: 'building',
-    postText: 'process'
-  })
 
   let results = ''
 
